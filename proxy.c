34c46cf2240af6f8ea9ebd162826d35d1987bbe4
LuLuPan <luning.pan@hotmail.com>
2013-08-14 20:39:58 +0800

    1. Implement Web cache. 2. Code refine

--- proxy.c
+++ proxy.c
@@ -25,6 +25,8 @@
 #define SET_NUM     1   //sets number
 #define SET_ENTRY   ((MAX_CACHE_SIZE / MAX_OBJECT_SIZE) / SET_NUM) //cache lines
 
+#define DEFAULT_HDRS 5
+
 static const char *user_agent = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 static const char *accept_msg = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n";
 static const char *accept_encoding = "Accept-Encoding: gzip, deflate\r\n";
@@ -32,6 +34,8 @@ static const char *connection = "Connection: close\r\n";
 static const char *proxy_connection = "Proxy-Connection: close\r\n";
 
 pthread_mutex_t  restrict_mutex;
+pthread_mutex_t  cache_lock; // lock when op on cache
+
 sem_t mutex;
 sem_t mutex_q;
 
@@ -56,16 +60,19 @@ struct _CACHE_LINE
     char tag[MAXLINE];
     char block[MAX_OBJECT_SIZE];
     PCACHE_LINE pNext;
+    int index;
 };
 
 PCACHE_LINE pCacheHeader = NULL;
 PCACHE_LINE pCacheCurr = NULL;
+PCACHE_LINE cache_lines[SET_ENTRY];
 
 
 /*unitility functions declaration*/
-int read_request(int connfd, char *req_buf, char *hostname, int *port);
+int read_request(int connfd, char *req_buf, char *uri, char *hostname, int *port);
 int forward_request(rio_t *rio, char *req_buf, int clientfd);
-int forward_response(rio_t *rio_server, char *resp_buf, int connfd);
+int forward_response(rio_t *rio_server, char *uri, char *resp_buf, int connfd);
+int insert_default_hdrs(char *req_buf, int UA, int AC, int AE, int CON, int PCON);
 void *proxy_thread(void *argp);
 int run_proxy(int connfd);
 int is_valid_fd(int fd);
@@ -107,57 +114,150 @@ int pop_fd_q()
     return -1;
 }
 
+/*allocate cache*/
 int init_cache()
 {
     PCACHE_LINE pCache = NULL;
-    PCACHE_LINE pCachePrev = NULL;
-    int i, j; 
-    for(i = 0; i < SET_NUM; i++)
+    int j;
+
+
+    for(j = 0; j < SET_ENTRY; j++)
     {
-        for(j = 0; j < SET_ENTRY; j++)
-        {
-            pCache = malloc(sizeof(CACHE_LINE));
-            memset(pCache, 0, sizeof(CACHE_LINE));
-            pCache->in_use = 0;
-            pCache->pNext = NULL;
-            if(i == 0 && j == 0) {
-                pCacheHeader = pCache;
-            }
-            else {
-                pCachePrev->pNext = pCache;
-            }
-            
-            pCachePrev = pCache;
+        pCache = Malloc(sizeof(CACHE_LINE));
+        memset(pCache, 0, sizeof(CACHE_LINE));
+        pCache->in_use = 0;
+        if(j == 0) {
+            pCacheHeader = pCache;
         }
-        
+
+        pCache->index = j;
+
+        cache_lines[j] = pCache;
     }
+        
     
     return 0;
 }
 
-
+/*destory cache*/
 int deinit_cache()
 {
-    int i, j;
+    int j;
     PCACHE_LINE cache = pCacheHeader;
-    PCACHE_LINE pCacheNext = NULL;
     
-    for(i = 0; i < SET_NUM; i++)
+    for(j = 0; j < SET_ENTRY; j++)
     {
-        for(j = 0; j < SET_ENTRY; j++)
-        {
-            pCacheNext = cache->pNext;
-            free(cache);
-            cache = pCacheNext;
-        }   
-    }
+        cache = cache_lines[j];
+        Free(cache);
+    }   
     
     return 0;
 }
+
+/* Manage LRU table and do evication
+     Recent-Accessed Cacheline is allocated head of LRU table
+     Need mutex lock since access concurrence racing
+  */
+int update_lru(int index)
+{
+    int i = 0;
+    PCACHE_LINE lru_cache = cache_lines[index];
+    
+    pthread_mutex_lock(&cache_lock);
+
+    if(index > (SET_ENTRY - 1))
+    {
+        printf("update_lru: cache internal error!\n");
+        return -1;
+    }
+    
+    for(i = index; i >= 1; i--)
+    {
+        cache_lines[i] = cache_lines[i-1];
+    }
+    
+    //Recent accessed cache
+    cache_lines[0] = lru_cache;
+
+    pthread_mutex_unlock(&cache_lock);
+
+    return 0;
+}
+
+
+/*store response in cache for future request*/
+int web_store(char *url, char *response)
+{
+    PCACHE_LINE pCache = pCacheHeader;
+    int i = 0;
+
+    for(i = 0; i < SET_ENTRY; i++)
+    {
+        pCache = cache_lines[i];
+        if(pCache == NULL) {
+            printf("cache internal error!\n");
+            return -1;
+        }
+
+        if(pCache->in_use == 0) {
+            strcpy(pCache->tag, url);
+            strcpy(pCache->block, response);
+            pCache->in_use = 1;
+            return 0;
+        }
+    }
+
+
+    /* miss, will do evication*/
+    if(i == SET_ENTRY) {
+        update_lru(SET_ENTRY-1);
+        pCache = cache_lines[0];
+        pCache->in_use = 1;
+        strcpy(pCache->tag, url);
+        strcpy(pCache->block, response);
+        return 0;
+    }
+    
+    return -1;
+}
+
+
+
+/* load response from cache instead of query from server
+  * input: url, response data pointer
+  * return: response data size
+*/
+int web_load(char *url, char* response)
+{
+    PCACHE_LINE pCache = pCacheHeader;
+    int i = 0;
+    if(response == NULL)
+        return -1;
+    
+    for(i = 0; i < SET_ENTRY; i++)
+    {
+        pCache = cache_lines[i];
+        if(pCache == NULL) {
+            printf("cache internal error!\n");
+            return -1;
+        }
+        
+        if(pCache->in_use && (strcmp(pCache->tag, url) == 0))
+        {
+            /* cache hit */
+            strcpy(response, pCache->block);
+            update_lru(i);
+            return 0;
+        }
+    }
+
+    return -1;
+}
+
 
 int main(int argc, char **argv)
 {
-    int listenfd, connfd, port;
+    int listenfd, port;
     struct sockaddr_in clientaddr;
     socklen_t clientlen;
     pthread_t tid;
@@ -194,14 +294,16 @@ int main(int argc, char **argv)
     
     while (1) {
         clientlen = sizeof(clientaddr);
-        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
-        insert_fd_q(connfd);
         /*thread timing issue could happen between accept and execuing thread
                  Normal: A:fd1->t1(fd1)->A:fd2->t2(fd2)
                  Abnormal: A:fd1->A:fd2->t1(fd2)->t2(fd2)
-                 Use fd task queue to avoid this by insert-consume
-                 */           
-        Pthread_create(&tid, NULL, proxy_thread, NULL);
+                 Use Malloc to avoid var race condition
+               */ 
+        int *connfd = Malloc(sizeof(int));
+        *connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        //insert_fd_q(connfd); //async task queue
+          
+        Pthread_create(&tid, NULL, proxy_thread, connfd);
     }
 
     Close(listenfd);
@@ -324,13 +426,48 @@ int parse_reqhdr(char *buf, char *hostname, int *port)
      return 0;
 }
 
-/*parse request line/headers, set default HEADER*/
-int read_request(int connfd, char *req_buf, char *hostname, int *port)
+int insert_default_hdrs(char *req_buf, int UA, int AC, int AE, int CON, int PCON)
 {
-    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+    char req_tmp[MAXLINE];
+    //printf("### insert_default_hdrs\n");
+    if(AE)
+    {
+        sprintf(req_tmp, "%s", user_agent);
+        strcat(req_buf, req_tmp);
+    }
+    if(AC)
+    {
+        sprintf(req_tmp, "%s", accept_msg);
+        strcat(req_buf, req_tmp);
+    }
+    if(AE)
+    {
+        sprintf(req_tmp, "%s", accept_encoding);
+        strcat(req_buf, req_tmp);
+    }
+    if(CON)
+    {
+        sprintf(req_tmp, "%s", connection);
+        strcat(req_buf, req_tmp);
+    }
+    if(PCON)
+    {
+        sprintf(req_tmp, "%s", proxy_connection);
+        strcat(req_buf, req_tmp);
+    }
+    
+    return 0;
+}
+
+
+/*parse request line/headers, set default HEADER*/
+int read_request(int connfd, char *req_buf, char *uri, char *hostname, int *port)
+{
+    char buf[MAXLINE], method[MAXLINE], version[MAXLINE];
     char req_tmp[MAXLINE];
     rio_t rio;
     int len = -1;
+    int UA = 0,AC = 0, AE = 0, CON = 0, PCON = 0;
     ssize_t n = 0;
     
     /* Read request line */
@@ -375,30 +512,35 @@ int read_request(int connfd, char *req_buf, char *hostname, int *port)
         {
             sprintf(req_tmp, "%s", user_agent);
             strcat(req_buf, req_tmp);
+            UA = 1;
             //DEBUG_PRINT(req_buf);
         }
         else if(find_string(buf, "Accept:"))
         {
             sprintf(req_tmp, "%s", accept_msg);
             strcat(req_buf, req_tmp);
+            AC = 1;
             //DEBUG_PRINT(req_buf);
         }
         else if(find_string(buf, "Accept-Encoding:"))
         {
             sprintf(req_tmp, "%s", accept_encoding);
             strcat(req_buf, req_tmp);
+            AE = 1;
             //DEBUG_PRINT(req_buf);
         }
         else if(find_string(buf, "Connection:"))
         {
             sprintf(req_tmp, "%s", connection);
             strcat(req_buf, req_tmp);
+            CON = 1;
             //DEBUG_PRINT(req_buf);
         }
         else if(find_string(buf, "Proxy-Connection:"))
         {
             sprintf(req_tmp, "%s", proxy_connection);
             strcat(req_buf, req_tmp);
+            PCON = 1;
             //DEBUG_PRINT(req_buf);
         }
                 
@@ -414,6 +556,8 @@ int read_request(int connfd, char *req_buf, char *hostname, int *port)
         //DEBUG_PRINT(buf);
         
     }
+
+    insert_default_hdrs(req_buf, UA, AC, AE, CON, PCON);
 
     //Todo: if without above request details, always send them here
 
@@ -448,16 +592,29 @@ int forward_request(rio_t *rio, char *req_buf, int clientfd)
 
 
 /*forward response (lines/headers/body) to client*/
-int forward_response(rio_t *rio_server, char *resp_buf, int connfd)
+int forward_response(rio_t *rio_server, char *uri, char *resp_buf, int connfd)
 {
     char response[MAX_OBJECT_SIZE];
     char resp_body[MAX_OBJECT_SIZE];
     char resp_line[MAXLINE];
+    int resp_size = 0;
     int length = 0;
     int body_size = 0;
     int content_len = -1;
     int cont_size = 0;
-
+#if 1
+    if((web_load(uri, response)) == 0)
+	{
+		printf("Cache hit!\n");
+		if(rio_writen(connfd, response, sizeof(response)) < 0)
+    	{
+        		fprintf(stderr, "rio_writen send cache response error\n");
+        		return -1;
+    	}
+    	memset(response, 0, sizeof(response));
+        return 0;
+	}
+#endif
     /*send reponse line and headers to client*/
     while((length = rio_readlineb(rio_server, resp_line, MAXLINE)) > 0)
     {
@@ -466,9 +623,6 @@ int forward_response(rio_t *rio_server, char *resp_buf, int connfd)
         if (rio_writen(connfd, resp_line, length) < 0) {
             fprintf(stderr, "Error: rio_writen() in forward_response header:  %s\n", strerror(errno));  
             DEBUG_PRINT("Error: rio_writen() in forward_response header\n");
-            /*write EPIPE error*/
-            //if(errno == EPIPE)
-            //    continue;
             return -1;
         }
         
@@ -484,12 +638,12 @@ int forward_response(rio_t *rio_server, char *resp_buf, int connfd)
                 fprintf(stderr, "Error get Content-Length: %s", resp_line);
                 DEBUG_PRINT("Error get Content-Length\n");
                 Close(connfd);
-                printf("@@@@ close fd: %d\n", connfd);
                 return -1;
             }
         }
 
         memset(resp_line, 0, sizeof(resp_line));
+        resp_size += length;
     }
 
 
@@ -508,15 +662,25 @@ int forward_response(rio_t *rio_server, char *resp_buf, int connfd)
 
     while((length = rio_readnb(rio_server, resp_body, cont_size)) > 0)
     {
+        strcat(response, resp_body);
         if (rio_writen(connfd, resp_body, length) < 0) {
             fprintf(stderr, "rio_writen in forward_response body error: %s!", strerror(errno));
             DEBUG_PRINT("rio_writen in forward_response body error!");
             return -1;
-            /*write EPIPE error*/
-            //if(errno == EPIPE)
-            //    continue;
         }
         body_size += length;
+    }
+#endif
+
+#if 1
+    resp_size += body_size;
+    if(resp_size <= MAX_OBJECT_SIZE)
+    {
+        if(web_store(uri, response) < 0) //store response in cache
+        {
+            printf("web_store, cache error!\n");
+            return -1;
+        }
     }
 #endif
 
@@ -563,6 +727,7 @@ int run_proxy(int connfd)
     char hostname[MAXLINE];
     char req_buf[MAX_OBJECT_SIZE];
     char resp_buf[MAX_OBJECT_SIZE];
+    char uri[MAXLINE]; 
     int port = 80;
     int clientfd;
     rio_t rio;
@@ -572,8 +737,8 @@ int run_proxy(int connfd)
     memset(resp_buf, 0 , MAX_OBJECT_SIZE);
 
     /* read request */
-    if(read_request(connfd, req_buf, hostname, &port) < 0) {
-        printf("close fd: %d, tid: %d\n", connfd, gettid());
+    if(read_request(connfd, req_buf, uri, hostname, &port) < 0) {
+        //printf("close fd: %d, tid: %d\n", connfd, gettid());
         Close(connfd);
         return -1;
     }
@@ -597,7 +762,7 @@ int run_proxy(int connfd)
         return -1;
     }
     
-    if(forward_response(&rio, resp_buf, connfd) < 0)
+    if(forward_response(&rio, uri, resp_buf, connfd) < 0)
     {
         printf("forward_response\n");
         fprintf(stderr, "Error: Send response to client failed !\n");
@@ -615,9 +780,10 @@ int run_proxy(int connfd)
 /* thread handler: do proxy*/
 void *proxy_thread(void *argp)
 {
-    int connfd; 
+    int connfd = *((int *)argp);
     Pthread_detach(pthread_self());
-    connfd = pop_fd_q();
+    Free(argp);
+    //connfd = pop_fd_q();
     //printf("### proxy_thread: %d, tid: %d\n", connfd, gettid());
     run_proxy(connfd);
 
